8fa4468ee43a4414c98e6e4bc86a29caf6b21f74
Mandeep Kaur <mdeep.kaur678@gmail.com>
2026-07-02 15:45:06 +0530

    Add style to navbar

--- src/pages/Header.jsx
+++ src/pages/Header.jsx
@@ -7,8 +7,8 @@ function Header(){
           <h1 className="font-bold text-xl text-pink-600">Tekup</h1>
           <div className="flex justify-between gap-24 font-bold">
             <NavLink to="/"className="hover:scale-105 hover:underline underline-offset-4 hover:text-pink-700">Home</NavLink>
-            <NavLink to="/about">About</NavLink>
-            <NavLink to="/contact">Contact</NavLink>
+            <NavLink to="/about" className="hover:scale-105 hover:underline underline-offset-4 hover:text-pink-700">About</NavLink>
+            <NavLink to="/contact" className="hover:scale-105 hover:underline underline-offset-4 hover:text-pink-700">Contact</NavLink>
           </div>
           <button className="mx-10 px-8 py-1 bg-gradient-to-r from-purple-900 to-pink-700 text-white rounded-lg hover:scale-105 transition-all duration-300">
             Get in Touch
