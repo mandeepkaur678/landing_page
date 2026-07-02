c85f6b77dd47eec6d92820453ccf21e17b259e51
Mandeep Kaur <mdeep.kaur678@gmail.com>
2026-07-02 15:43:18 +0530

    Add Layout route

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,24 @@
 import { Routes,Route } from "react-router-dom"
-import Header from "./pages/Header"
+import Layout from "./Layout"
 import Home from "./pages/Home"
 import About from "./pages/About"
 import Contact from "./pages/Contact"
-import Footer from "./pages/Footer"
+
 
 function App() {
  
 
   return (
     <>
-    <Header/>
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/about" element={<About />} />
-      <Route path="/contact" element={<Contact />} />
-    </Routes>
-    <Footer/>
-  </>
-  )
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/contact" element={<Contact />} />
+        </Route>
+      </Routes>
+    </>
+  );
 }
 
 export default App
